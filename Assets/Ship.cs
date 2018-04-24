a5dd2ebac90e51baea9d69cdcec3df6e1e3eb12d
NickMcG-PC <D11123772@mydit.ie>
2018-04-25 00:43:49 +0100

    got rotation

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -9,33 +9,30 @@ public class Ship : MonoBehaviour
 
 
 	//public List<Engine> MyEngines;
+	public GameObject sphere;
 	public bool HasPeople = false;
 	
 	public float EnginePower = 1.0f; // 1 should equal 1g of acceleration at 0.1 scale
 	
 	public float MaxEngineForce = 6000.0f; // multiplied by 1000 later
-	public float ShipMass = 2400000.0f;
+	public float ShipMass = 2400000.0f; // the mass of the ship
 
 	public float Acceleration = 0.0f;
-	//public float MyGs = 0.0f;
 	public float MaxGForce = 100.0f;
 	private const float OneG = 0.98f;
 
 	public Vector3 CurrentVelocity = Vector3.zero;
 	public Vector3 Target = Vector3.zero;
 
-	public Quaternion Look;
+	private Vector3 _currentRotation = Vector3.zero;
 
 	private float _wanderTimer = 0.0f;
 	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		if (HasPeople)
-		{
 			MaxGForce = 6.0f;
-		}
-
-		Look = transform.rotation;
 	}
 	
 	// Update is called once per frame
@@ -51,22 +48,26 @@ public class Ship : MonoBehaviour
 	{
 		if (Time.time < _wanderTimer) return;
 		
-		Target = new Vector3(Random.Range(-400,400),0.0f,Random.Range(-400,400));
+		Target = new Vector3(Random.Range(-400,400),Random.Range(-400,400),Random.Range(-400,400));
 
 		_wanderTimer = Time.time + Random.Range(1.0f, 7.0f);
+		sphere.transform.position = Target;
 	}
 
 	void PanToTarget()
 	{
 
 		Vector3 v3ToTarget = (Target - transform.position).normalized;
+		Vector3 myLook = transform.forward;
+		Vector3 desiredVector3 = v3ToTarget;
 		
-		Quaternion quatToTarget = Quaternion.Euler(v3ToTarget);
+		print(Vector3.Angle(myLook, desiredVector3) - 90);
+		
+		_currentRotation += desiredVector3 * Mathf.Min(Vector3.Angle(myLook,desiredVector3) - 90.0f,0.25f)* Time.deltaTime;
+		
+		myLook += _currentRotation;
 
-		Look = quatToTarget * Look;
-
-		transform.rotation = Look;
-
+		transform.forward = myLook;
 	}
 
 	void Accel()
