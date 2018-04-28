a16c627ed05aa40a9775c7915192df92fe41e831
NickMcG-PC <D11123772@mydit.ie>
2018-04-28 13:36:43 +0100

    kill me

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -2,29 +2,40 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.Assertions.Comparers;
 using UnityEngine.Timeline;
 
 public class Ship : MonoBehaviour
 {
 
-
+	[Header("General Values")]
 	//public List<Engine> MyEngines;
-	public GameObject sphere;
+	private const float OneG = .0098f;
+	public GameObject Sphere;
 	public bool HasPeople = false;
+	public float ShipMass = 240.0f; // the mass of the ship
+	public float ForceNeededForOneG = 2.352f; 
 	
-	public float EnginePower = 1.0f; // 1 should equal 1g of acceleration at 0.1 scale
+	[Header("Main Drive Values")]
+	public float MainEnginePower = 1.0f; // 1 = 1G of acceleration
+	public float MainDriveAcceleration = 0.0f;
+	public float MaxGForceAllowed = 100.0f;
+
+	[Header("RCS Values")] 
+	public float RcsPower = 1.0f;
+	public float RcsAcceleration = 0.0f;
+	public Vector3 RcsVector3 = Vector3.zero;
 	
-	public float MaxEngineForce = 6000.0f; // multiplied by 1000 later
-	public float ShipMass = 2400000.0f; // the mass of the ship
-
-	public float Acceleration = 0.0f;
-	public float MaxGForce = 100.0f;
-	private const float OneG = 0.98f;
-
+	
+	[Header("My Vector Values")]
 	public Vector3 CurrentVelocity = Vector3.zero;
-	public Vector3 Target = Vector3.zero;
-
-	private Vector3 _currentRotation = Vector3.zero;
+	public Vector3 CurrentRotation = Vector3.zero;
+	public Vector3 DesiredVector3 = Vector3.zero;
+	public Vector3 SteeringVector3 = Vector3.zero;
+	public float Speed = 0.0f;
+	
+	[Header("Targets")]
+	public Vector3 TargetPosition = Vector3.zero;
 
 	private float _wanderTimer = 0.0f;
 	
@@ -32,75 +43,119 @@ public class Ship : MonoBehaviour
 	void Start () 
 	{
 		if (HasPeople)
-			MaxGForce = 6.0f;
+			MaxGForceAllowed = 6.0f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		ControlShip();
-		FindTarget();
+		FindRandomTarget();
+		ReachTarget();
 		PanToTarget();
-		Accel();	
-	}
-	
-	void FindTarget()
-	{
-		if (Time.time < _wanderTimer) return;
-		
-		Target = new Vector3(Random.Range(-400,400),Random.Range(-400,400),Random.Range(-400,400));
-
-		_wanderTimer = Time.time + Random.Range(1.0f, 7.0f);
-		sphere.transform.position = Target;
+		CalcForwardVelocity();
+		Traversal();
 	}
 
-	void PanToTarget()
-	{
-
-		Vector3 v3ToTarget = (Target - transform.position).normalized;
-		Vector3 myLook = transform.forward;
-		Vector3 desiredVector3 = v3ToTarget;
-		
-		print(Vector3.Angle(myLook, desiredVector3) - 90);
-		
-		_currentRotation += desiredVector3 * Mathf.Min(Vector3.Angle(myLook,desiredVector3) - 90.0f,0.25f)* Time.deltaTime;
-		
-		myLook += _currentRotation;
-
-		transform.forward = myLook;
-	}
-
-	void Accel()
+	void ReachTarget()
 	{
 		
-		float actualForce = EnginePower * (MaxEngineForce *100);
-		Acceleration = (actualForce / ShipMass);
+		Vector3 toTarget = TargetPosition - transform.position;
+		float distance = toTarget.magnitude;
 		
-		Vector3 accelerationVector3 = Acceleration * transform.forward;
-		
-		CurrentVelocity += accelerationVector3 * Time.deltaTime;
+		if (distance < 10.0f)
+		{
+			MainEnginePower = 0;
+			
+			//enter combat maneuveurs
+			return;
+		}
 
-		transform.position += CurrentVelocity;
+		DesiredVector3 = toTarget / distance;
+
+		SteeringVector3 = DesiredVector3 - CurrentVelocity;
+
+		if (Vector3.Angle(transform.forward, SteeringVector3) - 90 < 1)
+		{
+			MainEnginePower = 10;
+		}
+		else
+		{
+			MainEnginePower = 0;
+		}
+		
 	}
 	
 	private void ControlShip()
 	{
 		if (Input.GetKey(KeyCode.W))
 		{
-			EnginePower += 0.5f * Time.deltaTime;
-			EnginePower = Mathf.Min(EnginePower, MaxGForce);
+			MainEnginePower += 0.5f * Time.deltaTime;
+			MainEnginePower = Mathf.Min(MainEnginePower, MaxGForceAllowed);
 		}
 
 		if (Input.GetKey(KeyCode.S))
 		{
-			EnginePower -= 0.5f * Time.deltaTime;
-			EnginePower = Mathf.Max(EnginePower, 0.0f);
+			MainEnginePower -= 0.5f * Time.deltaTime;
+			MainEnginePower = Mathf.Max(MainEnginePower, 0.0f);
 		}
 	}
+	
+	void FindRandomTarget()
+	{
+		if (Time.time < _wanderTimer) return;
+		
+		TargetPosition = new Vector3(Random.Range(-400,400),Random.Range(-400,400),Random.Range(-400,400));
 
-	private void OnDrawGizmos()
+		_wanderTimer = Time.time + Random.Range(1.0f, 7.0f);
+		Sphere.transform.position = TargetPosition;
+	}
+
+	void PanToTarget()
+	{
+		
+		CurrentRotation = Vector3.RotateTowards(CurrentRotation, SteeringVector3, 0.05f, 1);
+	}
+	
+	void CalcForwardVelocity()
+	{
+		
+		float actualForce = MainEnginePower * ForceNeededForOneG;
+		MainDriveAcceleration = (actualForce / ShipMass);
+		
+		Vector3 accelerationVector3 = MainDriveAcceleration * transform.forward;
+		
+		CurrentVelocity += accelerationVector3 * Time.deltaTime;
+	}
+
+	void CalcRCS()
+	{
+		float actualForce = RcsPower * ForceNeededForOneG;
+		RcsAcceleration = actualForce / ShipMass;
+		
+		Vector3 accelerationVector3 = RcsAcceleration * RcsVector3;
+
+		CurrentVelocity += accelerationVector3 * Time.deltaTime;
+	}
+	
+	void Traversal()
+	{
+		transform.forward += CurrentRotation;
+		transform.position += CurrentVelocity;
+	}
+
+	
+	public void OnDrawGizmos()
 	{
 		Gizmos.color = Color.red;
-		Gizmos.DrawSphere(Target, 30.0f);
+		Gizmos.DrawLine (transform.position, transform.position + (DesiredVector3 * 5));
+		
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine (transform.position, transform.position + (SteeringVector3 * 100));
+		
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine (transform.position, transform.position + (CurrentVelocity * 5));
+		
+		
 	}
+	
 }
